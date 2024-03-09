4fd5a5c83982e4aa2f013417f70d483394f57bca
MauricioPB00 <mauricioperinbecker@gmail.com>
2024-03-09 14:46:34 -0300

    foto baixando na pasta

---
+++ src/Controller/FileUploadController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FileUploadController extends AbstractController
+{
+    /**
+     * @Route("/upload-file", name="upload_file")
+     */
+    public function upload(Request $request)
+    {   
+        $file = $request->files->get('file');
+        if (!$file) {
+            return $this->json(['error' => 'No file uploaded'], 400);
+        }
+
+        try {
+            $fileName = 'userImg/' . uniqid() . '.' . $file->guessExtension();
+            $file->move($this->getParameter('kernel.project_dir') . '/public/assets/', $fileName);
+
+            return $this->json(['message' => 'File uploaded successfully', 'file_path' => $fileName]);
+        } catch (FileException $e) {
+
+            return $this->json(['error' => 'File upload failed'], 500);
+        }
+    }
+}
