fd3ccd3221ac70ac3354b406da09f5d06c689504
MauricioPB00 <mauricioperinbecker@gmail.com>
2024-03-17 18:08:39 -0300

    Back para adm ajustar a hora

--- src/Controller/AprovarController.php
+++ src/Controller/AprovarController.php
@@ -8,6 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\UserDateTimeRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
 
 class AprovarController extends AbstractController
 {
@@ -60,6 +61,83 @@ class AprovarController extends AbstractController
             return new JsonResponse(['message' => 'Registros atualizados com sucesso'], Response::HTTP_OK);
         } catch (\Exception $e) {
             return new JsonResponse(['error' => 'Erro ao atualizar registros: ' . $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * @Route("/api/user/approve/patch/update", name="api_user_approve_patch_update",  methods={"PATCH"})
+     */
+    public function approve(Request $request, UserDateTimeRepository $userDateTimeRepository): Response
+    {
+        $data = json_decode($request->getContent(), true);
+        if (!isset($data['ids']) || !is_array($data['ids'])) {
+            return new JsonResponse(['error' => 'IDs não fornecidos'], Response::HTTP_BAD_REQUEST);
+        }
+        $ids = $data['ids'];
+
+        try {
+            $registros = $userDateTimeRepository->findBy(['id' => $ids]);
+            foreach ($registros as $registro) {
+                $horaeditada = new \DateTime($registro->getHoraeditada());
+                $registro->setTime($horaeditada);
+                $registro->setUpdate(null);
+                $registro->setHoraeditada(null);
+            }
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->flush();
+            return new JsonResponse(['message' => 'Registros atualizados com sucesso'], Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => 'Erro ao atualizar registros: ' . $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * @Route("/api/user/approve/patch/insert", name="api_user_approve_patch_insert",  methods={"PATCH"})
+     */
+    public function approveInsert(Request $request, UserDateTimeRepository $userDateTimeRepository): Response
+    {
+        $data = json_decode($request->getContent(), true);
+        if (!isset($data['ids']) || !is_array($data['ids'])) {
+            return new JsonResponse(['error' => 'IDs não fornecidos'], Response::HTTP_BAD_REQUEST);
+        }
+        $ids = $data['ids'];
+
+        try {
+            $registros = $userDateTimeRepository->findBy(['id' => $ids]);
+            foreach ($registros as $registro) {
+                $registro->setInsert(null);
+            }
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->flush();
+            return new JsonResponse(['message' => 'Registros atualizados com sucesso'], Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => 'Erro ao atualizar registros: ' . $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * @Route("/api/user/approve/delete/insert", name="api_user_approve_delete_insert", methods={"POST"})
+     */
+    public function deleteInsertNoApprove(Request $request, UserDateTimeRepository $userDateTimeRepository, EntityManagerInterface $entityManager): Response
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if (!isset($data['ids']) || !is_array($data['ids'])) {
+            return new JsonResponse(['error' => 'IDs não fornecidos'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $ids = $data['ids'];
+
+        try {
+            $registros = $userDateTimeRepository->findBy(['id' => $ids]);
+            foreach ($registros as $registro) {
+                $entityManager->remove($registro);
+            }
+            $entityManager->flush();
+
+            return new JsonResponse(['message' => 'Registros não aprovados removidos com sucesso'], Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => 'Erro ao remover registros não aprovados: ' . $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 }
