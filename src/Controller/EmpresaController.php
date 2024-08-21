d95621ba1643d067eaf5a80a16c02f5e24ea0f3a
MauricioPB00 <mauricioperinbecker@gmail.com>
2024-08-20 23:22:56 -0300

    pdf para calculo de horas

--- src/Controller/EmpresaController.php
+++ src/Controller/EmpresaController.php
@@ -13,6 +13,7 @@ use App\Entity\HorasCalculadas;
 use App\Repository\CalculoRepository;
 use App\Repository\UserDateTimeRepository;
 use App\Repository\UserRepository;
+use App\Repository\HorasCalculadasRepository;
 
 
 class EmpresaController extends AbstractController
@@ -46,27 +47,69 @@ class EmpresaController extends AbstractController
     }
 
     /**
-     * @Route("/user/empresa/{userId}/usuario", name="api_user_empresa_buscarDadosUsuario", methods={"GET"})
-     */
-    public function buscarDadosUsuario($userId, UserRepository $userRepository, UserDateTimeRepository $userDateTimeRepository): Response
-    {
-        $user = $userRepository->find($userId);
+ * @Route("/user/empresa/{userId}/usuario", name="api_user_empresa_buscarDadosUsuario", methods={"GET"})
+ */
+public function buscarDadosUsuario($userId, UserRepository $userRepository, UserDateTimeRepository $userDateTimeRepository, HorasCalculadasRepository $horasCalculadasRepository): Response
+{
+    $user = $userRepository->find($userId);
 
-        if (!$user) {
-            return new JsonResponse(['error' => 'User not found'], Response::HTTP_NOT_FOUND);
-        }
-
-        $userData = [
-            'id' => $user->getId(),
-            'email' => $user->getEmail(),
-            'nome' => $user->getName(),
-            'cpf' => $user->getCpf(),
-            'rg' => $user->getRg(),
-            'cidade' => $user->getCidade(),
-            'job' => $user->getJob(),
-            'horTrab' =>$user->getHorTrab()
-        ];
-
-        return new JsonResponse($userData, Response::HTTP_OK);
+    if (!$user) {
+        return new JsonResponse(['error' => 'User not found'], Response::HTTP_NOT_FOUND);
     }
+    
+    // Dados do usuário
+    $userData = [
+        'id' => $user->getId(),
+        'email' => $user->getEmail(),
+        'nome' => $user->getName(),
+        'cpf' => $user->getCpf(),
+        'rg' => $user->getRg(),
+        'cidade' => $user->getCidade(),
+        'job' => $user->getJob(),
+        'horTrab' => $user->getHorTrab()
+    ];
+
+    $primeiroDiaMesAtual = new \DateTime('first day of this month');
+    $ultimoDiaMesAtual = new \DateTime('last day of this month');
+    
+    $userDateTimes = $userDateTimeRepository->findByMonth($primeiroDiaMesAtual, $ultimoDiaMesAtual);
+    
+    usort($userDateTimes, function ($a, $b) {
+        return $a->getUser()->getId() - $b->getUser()->getId();
+    });
+    
+    $formattedUserDateTimes = [];
+    foreach ($userDateTimes as $userDateTime) {
+        $formattedUserDateTimes[] = [
+            'id' => $userDateTime->getId(),
+            'date' => $userDateTime->getDate()->format('Y-m-d'),
+            'time' => $userDateTime->getTime()->format('H:i:s'),
+            'user_id' => $userDateTime->getUser()->getId(),
+        ];
+    }
+
+    $horasCalculadas = $horasCalculadasRepository->findBy(['user' => $userId]);
+
+    $formattedHorasCalculadas = [];
+    if ($horasCalculadas) {
+        foreach ($horasCalculadas as $horasCalculada) {
+            $formattedHorasCalculadas[] = [
+                'diasFaltados' => $horasCalculada->getDiasFaltados(),
+                'diasTrabalhados' => $horasCalculada->getDiasTrabalhados(),
+                'horasFaltando' => $horasCalculada->getHorasFaltando(),
+                'totalHorasDomingo' => $horasCalculada->getTotalHorasDomingo(),
+                'totalHorasSabado' => $horasCalculada->getTotalHorasSabado(),
+            ];
+        }
+    }
+
+    $responseData = [
+        'userData' => $userData,
+        'userDateTimes' => $formattedUserDateTimes,
+        'horasCalculadas' => $formattedHorasCalculadas,
+    ];
+    
+    return new JsonResponse($responseData, Response::HTTP_OK);
 }
+
+ }
